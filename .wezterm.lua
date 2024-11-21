31cb9e90d9c7ac7add314bcaebe72de86ebfdfd6
Ted Cook <33679293+tedcook94@users.noreply.github.com>
2024-11-20 18:45:00 -0600

    fix some terminal keybindings

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,13 +1,16 @@
-local wezterm = require('wezterm')
+local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
 -- Specify shell path
-config.default_prog = { '/usr/bin/zsh' }
+config.default_prog = { "/usr/bin/zsh" }
 
 -- Appearance settings
-config.color_scheme = 'nord'
+config.color_scheme = "nord"
 config.font_size = 10
 config.enable_tab_bar = false
+
+-- Fix some keybindings
+config.enable_kitty_keyboard = true
 
 return config
