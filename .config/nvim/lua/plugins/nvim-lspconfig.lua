c9fe98e5dca6bfffbb1fb4ddbc7d7b2ab9b06b0e
Ted Cook <33679293+tedcook94@users.noreply.github.com>
2024-12-04 09:17:57 -0600

    add typescript support

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -121,13 +121,8 @@ return {
       -- pyright = {},
       -- rust_analyzer = {},
       -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-      --
-      -- Some languages (like typescript) have entire language plugins that can be useful:
-      --    https://github.com/pmizio/typescript-tools.nvim
-      --
-      -- But for many setups, the LSP (`ts_ls`) will work just fine
-      -- ts_ls = {},
       gopls = {},
+      ts_ls = {},
       zls = {},
       lua_ls = {
         settings = {
