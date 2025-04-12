e6e02ff743e72370b89cf96d35893dba31c3d90c
Ted Cook <33679293+tedcook94@users.noreply.github.com>
2025-04-12 15:03:38 -0500

    add copilot to neovim

---
+++ .config/nvim/lua/plugins/codecompanion.lua
@@ -0,0 +1,16 @@
+-- CodeCompanion is a productivity tool which streamlines how you develop with LLMs, in Neovim.
+-- https://codecompanion.olimorris.dev/
+
+return {
+  {
+    'olimorris/codecompanion.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-treesitter/nvim-treesitter',
+    },
+    opts = {},
+  },
+  {
+    'github/copilot.vim',
+  },
+}
