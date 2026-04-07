6922191a197aeacb1073a3758a4223f49c9889b2
Ted Cook <33679293+tedcook94@users.noreply.github.com>
2026-04-07 16:35:23 -0500

    add octo neovim config

---
+++ .config/nvim/lua/plugins/octo.lua
@@ -0,0 +1,13 @@
+return {
+  'pwntester/octo.nvim',
+  cmd = 'Octo',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-telescope/telescope.nvim',
+    'nvim-tree/nvim-web-devicons',
+  },
+  opts = {
+    picker = 'telescope',
+    enable_builtin = true,
+  },
+}
