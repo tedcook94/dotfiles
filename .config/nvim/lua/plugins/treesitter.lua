d6216be47a6b17d1d79bd75bb5fdb74aa0cc6f4e
Ted Cook <33679293+tedcook94@users.noreply.github.com>
2026-04-18 10:43:58 -0500

    fix treesitter issues

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,37 +1,28 @@
 return { -- Highlight, edit, and navigate code
   'nvim-treesitter/nvim-treesitter',
+  branch = 'main',
+  lazy = false,
   build = ':TSUpdate',
-  main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-  -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-  opts = {
-    ensure_installed = {
-      'bash',
-      'c',
-      'diff',
-      'html',
-      'lua',
-      'luadoc',
-      'markdown',
-      'markdown_inline',
-      'query',
-      'vim',
-      'vimdoc',
-    },
-    -- Autoinstall languages that are not installed
-    auto_install = true,
-    highlight = {
-      enable = true,
-      -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-      --  If you are experiencing weird indenting issues, add the language to
-      --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-      additional_vim_regex_highlighting = { 'ruby' },
-    },
-    indent = { enable = true, disable = { 'ruby' } },
-  },
-  -- There are additional nvim-treesitter modules that you can use to interact
-  -- with nvim-treesitter. You should go explore a few and see what interests you:
-  --
-  --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-  --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-  --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+  config = function()
+    require('nvim-treesitter').setup()
+    vim.api.nvim_create_autocmd('FileType', {
+      callback = function(ev)
+        local lang = vim.treesitter.language.get_lang(vim.bo[ev.buf].filetype)
+        if not lang then return end
+        if pcall(vim.treesitter.language.add, lang) then
+          vim.treesitter.query.get:clear(lang, 'highlights')
+          pcall(vim.treesitter.start, ev.buf)
+        else
+          require('nvim-treesitter').install({ lang }):await(function(err)
+            if not err and vim.api.nvim_buf_is_valid(ev.buf) then
+              vim.schedule(function()
+                vim.treesitter.query.get:clear(lang, 'highlights')
+                pcall(vim.treesitter.start, ev.buf)
+              end)
+            end
+          end)
+        end
+      end,
+    })
+  end,
 }
